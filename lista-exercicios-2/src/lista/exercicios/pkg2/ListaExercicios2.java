e2bbc102bda951ac4a74db719ebbcfc1efbfa6d5
Matheus Soares <matheus.soares@prismainformatica.com.br>
2023-06-01 11:51:07 -0300

    Finalizada lista de exercicios do segundo bimestre

--- lista-exercicios-2/src/lista/exercicios/pkg2/ListaExercicios2.java
+++ lista-exercicios-2/src/lista/exercicios/pkg2/ListaExercicios2.java
@@ -13,25 +13,122 @@ import java.util.Scanner;
  */
 public class ListaExercicios2 {
 
-    private static int option = 1;
+    private static int tamanho;
     private static Scanner scan = new Scanner(System.in);
     private static ArrayList<Integer> array = new ArrayList();
+    private static boolean canRender = false;
+    private static int option;
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+
+        exibirMenu();
+
+    }
+
+    public static void exibirMenu() {
         Ordenacoes ord = new Ordenacoes();
-        while (option != 0) {
-            System.out.println("Digite os Numeros do Array, digite x para sair");
-            int opt = scan.nextInt();
-            if (opt != 0) {
-                array.add(opt);
-            }else{
-                option = opt;
+        System.out.println("                 MENU ORDENAÇÃO                         ");
+        System.out.println("╔═══════════════════════════════╗");
+        System.out.println("║ 1. Ordenação por Inserção                           ║");
+        System.out.println("║ 2. Ordenação por Seleção                            ║");
+        System.out.println("║ 3. Ordenação por Bolha                              ║");
+        System.out.println("║ 4. Pesquisar Número                                 ║");
+        System.out.println("║ 5. Sair                                             ║");
+        System.out.println("╚═══════════════════════════════╝");
+        option = scan.nextInt();
+        opcoesMenu(option, ord);
+    }
+
+    private static void getNumeros() {
+        array.clear();
+        System.out.println("Digite o tamanho do Vetor/Array: ");
+        try {
+            tamanho = scan.nextInt();
+            canRender = true;
+        } catch (Exception e) {
+            System.out.println("tem que ser um numero Inteiro");
+            canRender = false;
+        }
+        if (canRender) {
+            int i = 0;
+            while (i < tamanho) {
+                int num;
+                System.out.println("Digite os Numeros do Array, digite 0 para sair");
+                try {
+                    num = scan.nextInt();
+                    array.add(num);
+                } catch (Exception e) {
+                    System.out.println("tem que ser um numero Inteiro");
+                }
+                i++;
             }
         }
+    }
+
+    private static void insersao(Ordenacoes ord) {
+        getNumeros();
+        long tempoInicial = System.nanoTime();
         ord.ordenacaoInsersao(array);
+        long tempoFinal = System.nanoTime() - tempoInicial;
+        System.out.println("Tempo de execução em Milisegundos: " + tempoFinal);
+        exibirMenu();
+    }
+
+    private static void selecao(Ordenacoes ord) {
+        getNumeros();
+        long tempoInicial = System.nanoTime();
+        ord.ordenacaoSelecao(array);
+        long tempoFinal = System.nanoTime() - tempoInicial;
+        System.out.println("Tempo de execução em Milisegundos: " + tempoFinal);
+        exibirMenu();
+    }
+
+    private static void bolha(Ordenacoes ord) {
+        getNumeros();
+        long tempoInicial = System.nanoTime();
+        ord.ordenacaoBolha(array);
+        long tempoFinal = System.nanoTime() - tempoInicial;
+        System.out.println("Tempo de execução em Milisegundos: " + tempoFinal);
+        exibirMenu();
+    }
+
+    private static void pesquisa(Ordenacoes ord) {
+        int chave = 0;
+        System.out.println("Digite o numero a ser pesquisado: ");
+        try {
+            chave = scan.nextInt();
+        } catch (Exception e) {
+            System.out.println("tem que ser um numero Inteiro");
+        }
+        ord.pesquisaBinaria(array, chave);
+        exibirMenu();
+    }
+
+    private static void opcoesMenu(int opcao, Ordenacoes ord) {
+        switch (opcao) {
+            case 1:
+                insersao(ord);
+                break;
+            case 2:
+                selecao(ord);
+                break;
+            case 3:
+                bolha(ord);
+                break;
+            case 4:
+                pesquisa(ord);
+                break;
+            case 5:
+                System.out.println("Saindo ...");
+                break;
+
+            default:
+                System.out.println("Opção Invalida!");
+                exibirMenu();
+        }
     }
 
 }
--- lista-exercicios-2/src/lista/exercicios/pkg2/Ordenacoes.java
+++ lista-exercicios-2/src/lista/exercicios/pkg2/Ordenacoes.java
@@ -16,28 +16,137 @@ public class Ordenacoes {
 
     public void ordenacaoInsersao(ArrayList<Integer> original) {
         int chave, j;
+        int[] array = new int[original.size()];
         System.out.println("Você escolheu ordenação por Inserção");
         System.out.println("Vetor Original:");
         for (int i = 0; i < original.size(); i++) {
             System.out.print(original.get(i));
-
+            array[i] = original.get(i);
         }
-        for (int i = 1; i < original.size(); i++) {
-            chave = original.get(i);
 
-            for (j = i - 1; (j >= 0 && original.get(j) > chave); j--) {
+        for (int i = 1; i < array.length; i++) {
+            chave = array[i];
 
-                newArray.add((j + 1), original.get(j));
+            for (j = i - 1; (j >= 0 && array[j] > chave); j--) {
+
+                array[j + 1] = array[j];
 
             }
-            newArray.add((j + 1), chave);
+            array[j + 1] = chave;
         }
+
         System.out.println("");
         System.out.println("Vetor ordenado por Inserção: ");
-        for (int i = 0; i < newArray.size(); i++) {
-            System.out.print(newArray.get(i));
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
         }
         System.out.println("");
+    }
 
+    public void ordenacaoSelecao(ArrayList<Integer> original) {
+        int[] array = new int[original.size()];
+        System.out.println("Você escolheu ordenação por Seleção");
+        System.out.println("Vetor Original:");
+        for (int i = 0; i < original.size(); i++) {
+            System.out.print(original.get(i));
+            array[i] = original.get(i);
+        }
+        for (int i = 0; i < array.length - 1; i++) {
+            int posicaoMenor = i;
+
+            for (int j = i + 1; j < array.length; j++) {
+                if (array[j] < array[posicaoMenor]) {
+                    posicaoMenor = j;
+                }
+            }
+
+            if (posicaoMenor != i) {
+                int aux = array[i];
+                array[i] = array[posicaoMenor];
+                array[posicaoMenor] = aux;
+            }
+        }
+        System.out.println("");
+        System.out.println("Vetor ordenado por Seleção: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println("");
+    }
+
+    public void ordenacaoBolha(ArrayList<Integer> original) {
+        int[] array = new int[original.size()];
+        boolean houveTroca = true;
+        System.out.println("Você escolheu ordenação por Bolha");
+        System.out.println("Vetor Original:");
+        for (int i = 0; i < original.size(); i++) {
+            System.out.print(original.get(i));
+            array[i] = original.get(i);
+        }
+        while (houveTroca) {
+            houveTroca = false;
+
+            for (int i = 0; i < array.length - 1; i++) {
+                if (array[i] > array[i + 1]) {
+                    int aux = array[i];
+                    array[i] = array[i + 1];
+                    array[i + 1] = aux;
+                    houveTroca = true;
+                }
+            }
+        }
+        System.out.println("");
+        System.out.println("Vetor ordenado por Bolha: ");
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
+        System.out.println("");
+    }
+
+    public void pesquisaBinaria(ArrayList<Integer> original, int chave) {
+        int num, j;
+        int[] array = new int[original.size()];
+        for (int i = 0; i < original.size(); i++) {
+            array[i] = original.get(i);
+        }
+        for (int i = 1; i < array.length; i++) {
+            num = array[i];
+
+            for (j = i - 1; (j >= 0 && array[j] > num); j--) {
+
+                array[j + 1] = array[j];
+
+            }
+            array[j + 1] = num;
+        }
+        int posicao = pesquisa(chave, array);
+
+        if (posicao >= 0) {
+            System.out.println("O Elemento " + chave + " está localizado na "
+                    + "posição: " + posicao);
+        } else {
+            System.out.println(
+                    "O Elemento " + chave + " não foi localizado na lista");
+        }
+    }
+
+    private static int pesquisa(int chave, int[] vet) {
+        int esquerda, meio, direita;
+        esquerda = 0;
+        direita = vet.length - 1;
+
+        while (esquerda <= direita) {
+            meio = (esquerda + direita) / 2;
+            if (chave == vet[meio]) {
+                return meio;
+            }
+            if (chave > vet[meio]) {
+                esquerda = meio + 1;
+            } else {
+                direita = meio - 1;
+            }
+
+        }
+        return -1;
     }
 }
